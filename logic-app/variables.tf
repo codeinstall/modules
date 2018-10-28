7db0217f4bdb2a07e1e59648d5404dc7180d84f0
Finch <coder@galazs-mbp.home>
2018-10-28 14:28:59 +0000

    Initial commit

---
+++ logic-app/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The Prefix used for all resources in this example"
+  default     = "terraform"
+}
+
+variable "location" {
+  description = "The Azure Region in which the Logic App should be created"
+  default     = "West Europe"
+}
